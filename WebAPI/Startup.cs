b46cf644ad0b09c9b173ed54eac4d0199fc10190
Дмитрий <rdima.96@yande.ru>
2016-10-17 22:23:06 +0300

    Добавлена поддержка автоматического пересоздания базы данных.

--- WebAPI/Startup.cs
+++ WebAPI/Startup.cs
@@ -1,9 +1,11 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Data.Entity;
 using System.Linq;
 using Microsoft.Owin;
 using Owin;
 using Microsoft.Owin.Cors;
+using WebAPI.Models;
 
 [assembly: OwinStartup(typeof(WebAPI.Startup))]
 
@@ -13,6 +15,12 @@ namespace WebAPI
     {
         public void Configuration(IAppBuilder app)
         {
+            using (var db = new Model())
+            {
+                Database.SetInitializer(new DropCreateDatabaseIfModelChanges<Model>());
+                db.Database.Initialize(false);
+            }
+
             app.UseCors(CorsOptions.AllowAll);
             ConfigureAuth(app);
         }
