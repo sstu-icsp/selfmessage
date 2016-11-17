1a942931c6d5cb442d3924c1a69a0429fd3513e0
Dmitriy Radin <rdima.96@yandex.ru>
2016-11-17 08:36:05 +0300

    Доделана регистрация

--- SelfmessageSite/app/RegistrationController.js
+++ SelfmessageSite/app/RegistrationController.js
@@ -1,31 +1,27 @@
 ﻿
 app.controller("RegistrationController",
     function($scope, $log, $http) {
-        $scope.message = "Contact us! JK. This is just a demo.";
-        $scope.submitRegistrarion = function() {
-            var data = {
-                Email: $scope.Email,
-                Password: $scope.Password,
-                ConfirmPassword: $scope.ConfirmPassword
-            };
 
-            $http.post('http://localhost:10001/api/Account/Register', data)
-                .success(function(data) {
-                    $log.info("Registration success");
-                })
-                .error(function(data) {
-                    $log.error("Registration error");
-                });
+        $scope.user = { Email: "", Password: "", ConfirmPassword: "" };
+
+        $scope.registrtionError = false;
+        $scope.passwordNotMatch = false;
+
+        $scope.submitRegistrarion = function() {
+            if (window.registrtion_form.checkValidity()) {
+                if (!($scope.user.Password === $scope.user.ConfirmPassword)) {
+                    $scope.passwordNotMatch = true;
+                } else {
+                    $http.post("http://localhost:10001/api/Account/Register", $scope.user)
+                        .success(function(data) {
+                            $log.info("Registration success");
+                            window.location = "#/LogIn";
+                        })
+                        .error(function(data) {
+                            $log.error("Registration error");
+                            $scope.passwordNotMatch = true;
+                        });
+                }
+            }
         };
     });
-
-// ConfirmPassword
-function confirm() {
-    if (frm.confirmPassword.value != frm.password.value) {
-        alert("Пароли не совпадают");
-        return false
-    }
-    alert("Регистрация прошла успешно");
-    return true;
-    
-}
