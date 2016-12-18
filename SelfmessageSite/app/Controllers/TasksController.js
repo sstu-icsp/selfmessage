c895baa0e20b48e9d841d0ef3f555a6c726c440c
EndLessEmbrace <EndLessEmbrace@yandex.ru>
2016-12-18 13:48:00 +0400

    Сверстали макет добавления задач

--- SelfmessageSite/app/Controllers/TasksController.js
+++ SelfmessageSite/app/Controllers/TasksController.js
@@ -1,4 +1,63 @@
 ﻿app.controller('TasksController',
-    function($scope, $http, $log, $rootScope) {
+    function($scope, $http, $log, $rootScope,TaskService) {
 
+        if ($rootScope.isAuthorize !== true) {
+            window.location = "#/";
+        }
+        TaskService.getAllData()
+              .then(
+                  function (data) {
+                      $log.info(data);
+                      $scope.Details = data.reverse();
+                  },
+                  function (errorData) {
+                      $scope.errorMessage = errorData.data.error_description;
+                  });
+
+
+        $scope.getAllData = function () {
+            TaskService.getAllData()
+            .then(
+                function (data) {
+                    $log.info(data);
+                    $scope.Details = data.reverse();
+                },
+                function (errorData) {
+                    $scope.errorMessage = errorData.data.error_description;
+                });
+        }
+
+        $scope.taskData = {
+            Name: "",
+            Importance: "",
+            About: "",
+            StartTime: "",
+            EndTime: "",
+            
+
+        }
+        $scope.sendData = function () {
+            TaskService.sendData($scope.noteData)
+            .then(
+                function (data) {
+                    $log.info(data);
+                    $scope.PostDaraResponse = data;
+                    $scope.noteData = {};
+
+                    TaskService.getAllData()
+                        .then(
+                            function (data) {
+                                $log.info(data);
+                                $scope.Details = data.reverse();;
+                            },
+                            function (errorData) {
+                                $scope.errorMessage = errorData.data.error_description;
+                            });
+
+                },
+                function (errorData) {
+                    $scope.errorMessage = errorData.data.error_description;
+                }
+            );
+        }
     });
---
+++ SelfmessageSite/app/Services/TaskService.js
@@ -0,0 +1,96 @@
+﻿app.factory("TaskService",
+    function ($q, $log, $http, $httpParamSerializerJQLike) {
+
+        var REST_SERVICE_URI = "http://localhost:9343/";
+
+
+        var factory = {
+            getAllData: getAllData,
+            sendData: sendData
+            //deleteNote: deleteNote,
+            //searchData: searchData
+        };
+
+        return factory;
+
+
+        //Вывод ВСЕХ записей
+        function getAllData() {
+            var defered = $q.defer();
+            $http.get(REST_SERVICE_URI + 'api/tasks')
+                .then(
+                    function (response) {
+                        $log.debug(response);
+                        defered.resolve(response.data);
+                    },
+                    function (errorResponse) {
+                        $log.debug(errorResponse);
+                        defered.reject(errorResponse);
+                    }
+
+                );
+            return defered.promise;
+        };
+
+
+        //Уделанеи записи по id
+        //api/notes/{id}
+       /* function deleteNote(id) {
+            var defered = $q.defer();
+            $http.delete(REST_SERVICE_URI + 'api/notes/' + id)
+                .then(
+                    function (response) {
+                        $log.debug(response);
+                        defered.resolve(response.data);
+                    },
+                    function (errorResponse) {
+                        $log.debug(errorResponse);
+                        defered.reject(errorResponse);
+                    }
+
+                );
+            return defered.promise;
+        };
+
+        //Поиск записи по тегу
+        //api/notes/byname?notename=name
+        //name заменяется на название тэга
+        function searchData(findData) {
+            var defered = $q.defer();
+            $http.get(REST_SERVICE_URI + 'api/notes/byname?notename=' + findData.Name)
+                .then(
+                    function (response) {
+                        $log.debug(response);
+                        defered.resolve(response.data);
+                    },
+                    function (errorResponse) {
+                        $log.debug(errorResponse);
+                        defered.reject(errorResponse);
+                    }
+
+                );
+            return defered.promise;
+        };
+
+        */
+        //Добавление новой записи
+        function sendData(sendData) {
+            var defered = $q.defer();
+            $http.post(REST_SERVICE_URI + "api/tasks", sendData)
+             .then(
+                        function (response) {
+                            $log.debug(response);
+                            defered.resolve(response.data);
+                        },
+                        function (errorResponse) {
+                            $log.error("TaskService sendData error");
+                            $log.error(errorResponse);
+                            defered.reject(errorResponse);
+                        });
+            return defered.promise;
+        };
+
+
+
+
+    });
