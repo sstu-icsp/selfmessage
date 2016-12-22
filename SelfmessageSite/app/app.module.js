04120890a859a31f9401f1511c9f9358bc18610c
Dmitriy Radin <rdima.96@yandex.ru>
2016-12-22 20:27:32 +0400

    чямсчям

--- SelfmessageSite/app/app.module.js
+++ SelfmessageSite/app/app.module.js
@@ -21,11 +21,11 @@ app.config(function($routeProvider) {
             templateUrl: "../Pages/Notes.html",
             controller: "NotesController"
         })
-    .when("/Tasks",
-    {
-        templateUrl: "../Pages/Tasks.html",
-        controller: "TasksController"
-    });
+        .when("/Tasks",
+        {
+            templateUrl: "../Pages/Tasks.html",
+            controller: "TasksController"
+        });
 });
 
 app.run(function ($http, $cookieStore, $log, UserService, $rootScope) {
